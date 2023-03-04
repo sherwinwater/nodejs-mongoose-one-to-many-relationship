bfa7647d9c18c133fa52576c3379f32820c4fc13
Shuwen <swang@qmo.io>
2023-03-03 19:49:26 -0500

    add express

--- src/server.js
+++ src/server.js
@@ -1,153 +1,56 @@
-const mongoose = require("mongoose");
+const express = require('express')
+const { createComment, getAllBooks } = require('./services')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const db = require("./models");
+const app = express()
 
-const createTutorial = function(tutorial) {
-  return db.Tutorial.create(tutorial).then(docTutorial => {
-    console.log("\n>> Created Tutorial:\n", docTutorial);
-    return docTutorial;
-  });
-};
-
-const createImage = function(tutorialId, image) {
-  return db.Image.create(image).then(docImage => {
-    console.log("\n>> Created Image:\n", docImage);
-    return db.Tutorial.findByIdAndUpdate(
-      tutorialId,
-      {
-        $push: {
-          images: {
-            _id: docImage._id,
-            url: docImage.url,
-            caption: docImage.caption
-          }
-        }
-      },
-      { new: true, useFindAndModify: false }
-    );
-  });
-};
-
-// const createImage = function(tutorialId, image) {
-//   console.log("\n>> Add Image:\n", image);
-//   return db.Tutorial.findByIdAndUpdate(
-//     tutorialId,
-//     {
-//       $push: {
-//         images: {
-//           url: image.url,
-//           caption: image.caption
-//         }
-//       }
-//     },
-//     { new: true, useFindAndModify: false }
-//   );
-// };
-
-const createComment = function(tutorialId, comment) {
-  return db.Comment.create(comment).then(docComment => {
-    console.log("\n>> Created Comment:\n", docComment);
-
-    return db.Tutorial.findByIdAndUpdate(
-      tutorialId,
-      { $push: { comments: docComment._id } },
-      { new: true, useFindAndModify: false }
-    );
-  });
-};
-
-const createCategory = function(category) {
-  return db.Category.create(category).then(docCategory => {
-    console.log("\n>> Created Category:\n", docCategory);
-    return docCategory;
-  });
-};
-
-const addTutorialToCategory = function(tutorialId, categoryId) {
-  return db.Tutorial.findByIdAndUpdate(
-    tutorialId,
-    { category: categoryId },
-    { new: true, useFindAndModify: false }
-  );
-};
-
-const getTutorialWithPopulate = function(id) {
-  return db.Tutorial.findById(id)
-    .populate("comments", "-_id -__v")
-    .populate("category", "name -_id")
-    .select("-images._id -__v");
-};
-
-const getTutorialsInCategory = function(categoryId) {
-  return db.Tutorial.find({ category: categoryId })
-    .populate("category", "name -_id")
-    .select("-comments -images -__v");
-};
-
-const run = async function() {
-  var tutorial = await createTutorial({
-    title: "MongoDB One-to-Many Relationship example",
-    author: "bezkoder.com"
-  });
-
-  tutorial = await createImage(tutorial._id, {
-    path: "sites/uploads/images/mongodb.png",
-    url: "https://bezkoder.com/images/mongodb.png",
-    caption: "MongoDB Database",
-    createdAt: Date.now()
-  });
-  console.log("\n>> Tutorial:\n", tutorial);
-
-  tutorial = await createImage(tutorial._id, {
-    path: "sites/uploads/images/one-to-many.png",
-    url: "https://bezkoder.com/images/one-to-many.png",
-    caption: "One to Many Relationship",
-    createdAt: Date.now()
-  });
-  console.log("\n>> Tutorial:\n", tutorial);
-
-  tutorial = await createComment(tutorial._id, {
-    username: "jack",
-    text: "This is a great tutorial.",
-    createdAt: Date.now()
-  });
-  console.log("\n>> Tutorial:\n", tutorial);
-
-  tutorial = await createComment(tutorial._id, {
-    username: "mary",
-    text: "Thank you, it helps me alot.",
-    createdAt: Date.now()
-  });
-  console.log("\n>> Tutorial:\n", tutorial);
-
-  var category = await createCategory({
-    name: "Node.js",
-    description: "Node.js tutorial"
-  });
-
-  tutorial = await addTutorialToCategory(tutorial._id, category._id);
-  console.log("\n>> Tutorial:\n", tutorial);
-
-  tutorial = await getTutorialWithPopulate(tutorial._id);
-  console.log("\n>> populated Tutorial:\n", tutorial);
-
-  var newTutorial = await createTutorial({
-    title: "Mongoose tutorial with examples",
-    author: "bezkoder.com"
-  });
-
-  await addTutorialToCategory(newTutorial._id, category._id);
-
-  var tutorials = await getTutorialsInCategory(category._id);
-  console.log("\n>> all Tutorials in Cagetory:\n", tutorials);
-};
+app.use(express.json())
+app.use(cors())
+app.listen(3000)
 
 mongoose
-  .connect("mongodb://localhost/bezkoder_db", {
+  .connect('mongodb://rootuser:rootpass@localhost:27017/books', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log("Successfully connect to MongoDB."))
-  .catch(err => console.error("Connection error", err));
+  .then(() => console.log('Successfully connect to MongoDB.'))
+  .catch(err => console.error('Connection error', err))
 
-run();
+app.get('/books', async (req, res) => {
+  const todos = await getAllBooks()
+  console.log(req.url, todos)
+
+  res.json(todos)
+})
+
+
+// POST /book/:id/comment - add a new comment to a book
+app.post('/book/:id/comment', async (req, res) => {
+  const bookId = req.params.id;
+  const commentData = req.body;
+
+  try {
+    // Find the book by ID
+    const book = await Tutorial.findById(bookId);
+    if (!book) {
+      return res.status(404).send({ message: 'Book not found' });
+    }
+
+    // Create a new comment and add it to the book's comments array
+    const comment = await Comment.create(commentData);
+
+    book.comments.push(comment);
+
+    // Save the book with the new comment
+    await book.save();
+
+    // Return the updated book object
+    res.send(book);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Internal server error' });
+  }
+});
---
+++ src/services.js
@@ -0,0 +1,163 @@
+const db = require("./models");
+
+const createTutorial = function(tutorial) {
+  return db.Tutorial.create(tutorial).then(docTutorial => {
+    console.log("\n>> Created Tutorial:\n", docTutorial);
+    return docTutorial;
+  });
+};
+
+const createImage = function(tutorialId, image) {
+  return db.Image.create(image).then(docImage => {
+    console.log("\n>> Created Image:\n", docImage);
+    return db.Tutorial.findByIdAndUpdate(
+      tutorialId,
+      {
+        $push: {
+          images: {
+            _id: docImage._id,
+            url: docImage.url,
+            caption: docImage.caption
+          }
+        }
+      },
+      { new: true, useFindAndModify: false }
+    );
+  });
+};
+
+// const createImage = function(tutorialId, image) {
+//   console.log("\n>> Add Image:\n", image);
+//   return db.Tutorial.findByIdAndUpdate(
+//     tutorialId,
+//     {
+//       $push: {
+//         images: {
+//           url: image.url,
+//           caption: image.caption
+//         }
+//       }
+//     },
+//     { new: true, useFindAndModify: false }
+//   );
+// };
+
+const createComment = function(tutorialId, comment) {
+  return db.Comment.create(comment).then(docComment => {
+    console.log("\n>> Created Comment:\n", docComment);
+
+    return db.Tutorial.findByIdAndUpdate(
+      tutorialId,
+      { $push: { comments: docComment._id } },
+      { new: true, useFindAndModify: false }
+    );
+  });
+};
+
+const createComment2 = async function(tutorialId, comment) {
+  const docComment = await db.Comment.create(comment);
+  console.log("\n>> Created Comment:\n", docComment);
+
+  const tutorial = await db.Tutorial.findById(tutorialId);
+  tutorial.comments.push(docComment._id);
+  const updatedTutorial = await tutorial.save();
+
+  return updatedTutorial;
+};
+
+
+const createCategory = function(category) {
+  return db.Category.create(category).then(docCategory => {
+    console.log("\n>> Created Category:\n", docCategory);
+    return docCategory;
+  });
+};
+
+const addTutorialToCategory = function(tutorialId, categoryId) {
+  return db.Tutorial.findByIdAndUpdate(
+    tutorialId,
+    { category: categoryId },
+    { new: true, useFindAndModify: false }
+  );
+};
+
+const getTutorialWithPopulate = function(id) {
+  return db.Tutorial.findById(id)
+    .populate("comments", "-_id -__v")
+    .populate("category", "name -_id")
+    .select("-images._id -__v");
+};
+
+const getAllBooks= function() {
+  return db.Tutorial.find()
+};
+
+const getTutorialsInCategory = function(categoryId) {
+  return db.Tutorial.find({ category: categoryId })
+    .populate("category", "name -_id")
+    .select("-comments -images -__v");
+};
+
+const run = async function() {
+  var tutorial = await createTutorial({
+    title: "MongoDB One-to-Many Relationship example",
+    author: "bezkoder.com"
+  });
+
+  tutorial = await createImage(tutorial._id, {
+    path: "sites/uploads/images/mongodb.png",
+    url: "https://bezkoder.com/images/mongodb.png",
+    caption: "MongoDB Database",
+    createdAt: Date.now()
+  });
+  console.log("\n>> Tutorial:\n", tutorial);
+
+  tutorial = await createImage(tutorial._id, {
+    path: "sites/uploads/images/one-to-many.png",
+    url: "https://bezkoder.com/images/one-to-many.png",
+    caption: "One to Many Relationship",
+    createdAt: Date.now()
+  });
+  console.log("\n>> Tutorial:\n", tutorial);
+
+  tutorial = await createComment(tutorial._id, {
+    username: "jack",
+    text: "This is a great tutorial.",
+    createdAt: Date.now()
+  });
+  console.log("\n>> Tutorial:\n", tutorial);
+
+  tutorial = await createComment(tutorial._id, {
+    username: "mary",
+    text: "Thank you, it helps me alot.",
+    createdAt: Date.now()
+  });
+  console.log("\n>> Tutorial:\n", tutorial);
+
+  var category = await createCategory({
+    name: "Node.js",
+    description: "Node.js tutorial"
+  });
+
+  tutorial = await addTutorialToCategory(tutorial._id, category._id);
+  console.log("\n>> Tutorial:\n", tutorial);
+
+  tutorial = await getTutorialWithPopulate(tutorial._id);
+  console.log("\n>> populated Tutorial:\n", tutorial);
+
+  var newTutorial = await createTutorial({
+    title: "Mongoose tutorial with examples",
+    author: "bezkoder.com"
+  });
+
+  await addTutorialToCategory(newTutorial._id, category._id);
+
+  var tutorials = await getTutorialsInCategory(category._id);
+  console.log("\n>> all Tutorials in Cagetory:\n", tutorials);
+};
+
+module.exports={
+  getAllBooks,
+  createComment,
+  createComment2
+}
